856003c3e7d34a6d40dbd3557596eae5a2ac22c7
Cap0n3 <cap0n3@protonmail.com>
2022-02-09 15:32:36 +0100

    Started design of Client detail page

--- mgr-app-frontend/src/pages/ClientDetail.js
+++ mgr-app-frontend/src/pages/ClientDetail.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { useNavigate} from "react-router-dom";
 import { useParams } from 'react-router-dom'
 import AuthContext from "../context/AuthContext";
+import { MainWrapper, Aside, Section } from "./pagesStyles/ClientDetail.style";
 
 const Cloud = () => {
 	const [clientData, setData ] = useState([])
@@ -31,33 +32,12 @@ const Cloud = () => {
 	}
 	
 	return (
-		<>
-			<table>
-				<thead>
-					<tr>
-						<th scope="col">Prénom</th>
-						<th scope="col">Nom</th>
-						<th scope="col">Jour du cours</th>
-						<th scope="col">Heure du cours</th>
-						<th scope="col">Durée du cours</th>
-						<th scope="col">Fréquence du cours</th>
-						<th scope="col">Instument</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>{clientData.first_name}</td>
-						<td>{clientData.last_name}</td>
-						<td>{clientData.lesson_day}</td>
-						<td>{clientData.lesson_hour}</td>
-						<td>{clientData.lesson_duration} minutes</td>
-						<td>{clientData.lesson_frequency}</td>
-						<td>{clientData.instrument}</td>
-						{/* <td><EditButton name="update" value={clientID} onClick={handleUpdateBtn}>Update</EditButton></td> */}
-					</tr>
-				</tbody>
-			</table>		
-		</>);
+		
+		<MainWrapper>
+			<Aside></Aside>
+			<Section></Section>
+		</MainWrapper>
+	);
 }
 
 export default Cloud;
---
+++ mgr-app-frontend/src/pages/pagesStyles/ClientDetail.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+
+export const MainWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    border: 1px solid red;
+`;
+
+export const Aside = styled.aside`
+    width: 400px;
+    height: 100%;
+    box-sizing: border-box;
+    padding: 15px;
+    border: 1px solid green;
+`;
+
+export const Section = styled.section`
+    border: 1px solid blue;
+    width: 100%;
+    height: 100%;
+    padding: 15px;
+    box-sizing: border-box;
+`;
