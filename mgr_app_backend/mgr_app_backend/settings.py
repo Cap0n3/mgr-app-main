4680b935a47024ec766a0c48e31c5f17200f8245
Cap0n3 <cap0n3@protonmail.com>
2022-07-19 20:30:21 +0200

    Added env variables to backend

--- mgr_app_backend/mgr_app_backend/settings.py
+++ mgr_app_backend/mgr_app_backend/settings.py
@@ -13,22 +13,30 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import environ
+
+# Env variable setup
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_l6#*km8say(pqh!84y0@82spx8g=2vohd$6%(9oa21!!@#_2%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
